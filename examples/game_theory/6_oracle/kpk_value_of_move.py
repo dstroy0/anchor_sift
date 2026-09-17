7b87963311cd5159d05e841483f04efee220cd08
dstroy0 (Douglas Quigg) <dquigg123@gmail.com>
2026-09-17 02:52:05 -0400

    tools catalog note

--- examples/game_theory/6_oracle/kpk_value_of_move.py
+++ examples/game_theory/6_oracle/kpk_value_of_move.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # anchor_sift - Copyright (C) 2026 Douglas Quigg (dstroy0) <dquigg123@gmail.com>
 # SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Commercial OR LicenseRef-Educational
+# Catalog: GAM-6-003
 #
 # The value of the move in K+P vs K, solved exactly, checked against published endgame theory.
 #
