d40cc0d589ca146aa2fd475e0f5772a0b45ddc30
dstroy0 (Douglas Quigg) <dquigg123@gmail.com>
2026-09-16 21:47:02 -0400

    tools buildengine feature

--- maint/engine/build_engine.ps1
+++ maint/engine/build_engine.ps1
@@ -162,6 +162,7 @@ if ($haveCuda)
 # anchor_sift_kernel, which is the whole engine in one translation unit.
 $targets = @("anchor_sift_kernel", "anchor_sift_kernel_counted", "anchor_raster", "anchor_render",
              "anchor_exact_portable", "test_steer", "test_adversarial", "test_arm_agreement",
+             "test_o2_spawn",
              "bench_steer_arms", "bench_raster", "bench_exact_arms", "bench_exact",
              "bench_dispatch", "bench_coherence", "bench_scaling_reads", "bench_scaling_cycles")
 Write-Host "[*] building"
@@ -182,7 +183,7 @@ if ($BuildOnly)
 }
 
 $failed = 0
-foreach ($grader in @("test_steer", "test_adversarial", "test_arm_agreement", "bench_steer_arms", "bench_raster", "bench_exact_arms"))
+foreach ($grader in @("test_steer", "test_adversarial", "test_arm_agreement", "test_o2_spawn", "bench_steer_arms", "bench_raster", "bench_exact_arms"))
 {
     $exe = Join-Path $build "$grader.exe"
     if (-not (Test-Path $exe))
